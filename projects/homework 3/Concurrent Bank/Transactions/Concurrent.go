b448a995f085f2fbaff593c582296c2c1f711803
juancespinozazumbado <espinozajuanki@gmail.com>
2024-12-17 19:37:29 -0600

    fix go routines slep
    mutex.Unlock() in a for range usning defer are a deadlok

--- projects/homework 3/Concurrent Bank/Transactions/Concurrent.go
+++ projects/homework 3/Concurrent Bank/Transactions/Concurrent.go
@@ -1,31 +1,110 @@
 package transactions
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 type TransactionReceiver struct {
-	InputChannel  chan Transaction
-	OutputChannel chan Transaction
+	InputChannel  chan *Transaction
+	OutputChannel chan *Transaction
 }
 
-type TransactionProcessor struct {
-	InputChannel   chan Transaction
-	OutputChannel  chan Transaction
-	AccountBalance int
-	Mutex          sync.Mutex
+func NewTransactionReceiver(ch chan *Transaction, cr chan *Transaction) *TransactionReceiver {
+	reciever := &TransactionReceiver{
+		InputChannel:  ch,
+		OutputChannel: cr,
+	}
+	go reciever.start()
+	return reciever
 }
 
-func (t *TransactionProcessor) Start() {
+func (t *TransactionReceiver) start() {
 
 	/// method to be implemented
 
+	for {
+		select {
+		case trans := <-t.InputChannel:
+
+			fmt.Printf("Recieved Transaction: %+v \n", trans)
+
+			t.OutputChannel <- trans
+			//close(t.OutputChannel)
+		}
+
+	}
+
 }
+
+////////// Processor  ////////////////////
+
+var Mutex sync.Mutex
+
+type TransactionProcessor struct {
+	InputChannel   chan *Transaction
+	OutputChannel  chan *TransactionResponse
+	AccountBalance int
+}
+
+func NewTransactionProcessor(ch chan *Transaction, cr chan *TransactionResponse) *TransactionProcessor {
+	processor := &TransactionProcessor{
+		InputChannel:  ch,
+		OutputChannel: cr,
+	}
+	go processor.start()
+	return processor
+}
+
+func (t *TransactionProcessor) start() {
+
+	/// method to be implemented
+	for trans := range t.InputChannel {
+		Mutex.Lock()
+
+		if trans.Type == "whithdraw" {
+			if trans.Amount > t.AccountBalance {
+				t.OutputChannel <- &TransactionResponse{TransactionId: trans.ID, Succes: false, NewBalance: 0, Message: "insuficient fonds !"}
+				Mutex.Unlock()
+				continue
+			}
+
+			t.AccountBalance -= trans.Amount
+			t.OutputChannel <- &TransactionResponse{TransactionId: trans.ID, Succes: true, NewBalance: t.AccountBalance, Message: "withdraw succesfully!"}
+
+		} else {
+			t.AccountBalance += trans.Amount
+			t.OutputChannel <- &TransactionResponse{TransactionId: trans.ID, Succes: true, NewBalance: t.AccountBalance, Message: "deposit succesfully!"}
+
+		}
+		Mutex.Unlock()
+
+		fmt.Printf("Processed transaction %+v \n", trans)
+	}
+
+	//}
+
+}
+
+////////// Reposnse Handler  ////////////////////
 
 type ResponseHandler struct {
-	InputChannel chan TransactionResponse
+	InputChannel chan *TransactionResponse
 }
 
-func (t *ResponseHandler) Start() {
+func NewResponseHandler(ch chan *TransactionResponse, wg *sync.WaitGroup) *ResponseHandler {
+	handler := &ResponseHandler{
+		InputChannel: ch,
+	}
 
-	//method to be implemented
+	go handler.start(wg)
+	return handler
+}
 
+func (t *ResponseHandler) start(wg *sync.WaitGroup) {
+	for resp := range t.InputChannel {
+		fmt.Printf("Transaccion result %+v \n", resp)
+
+		wg.Done()
+	}
 }
--- projects/homework 3/Concurrent Bank/main.go
+++ projects/homework 3/Concurrent Bank/main.go
@@ -1,10 +1,37 @@
 package main
 
-import "fmt"
+import (
+	transactions "ConcurrentBank/Transactions"
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func main() {
 
-	fmt.Println("Hellow world")
+	trascInvCHannel := make(chan *transactions.Transaction)
+	trascOutCHannel := make(chan *transactions.Transaction)
+	ResponseChan := make(chan *transactions.TransactionResponse)
 
-	
+	handler := transactions.NewResponseHandler(ResponseChan, &wg)
+	processor := transactions.NewTransactionProcessor(trascOutCHannel, ResponseChan)
+	reciever := transactions.NewTransactionReceiver(trascInvCHannel, trascOutCHannel)
+
+	fmt.Printf("Reciever: %+v\n", reciever)
+	fmt.Printf("Processor: %+v\n", processor)
+	fmt.Printf("Hanlder: %+v\n", handler)
+
+	fmt.Println("Process started....")
+
+	// sequence of transactions
+	wg.Add(4)
+
+	trascInvCHannel <- &transactions.Transaction{ID: "1234", Type: "deposit", Amount: 200}
+	trascInvCHannel <- &transactions.Transaction{ID: "454545", Type: "deposit", Amount: 300}
+	trascInvCHannel <- &transactions.Transaction{ID: "1234", Type: "whithdraw", Amount: 1000}
+	trascInvCHannel <- &transactions.Transaction{ID: "1234", Type: "whithdraw", Amount: 400}
+
+	wg.Wait() // wait
+
 }
